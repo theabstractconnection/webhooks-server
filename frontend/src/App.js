b91374d78fcf2908e557150354c7f824e9a481ad
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-19 17:47:48 +0100

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+import "./App.css";
+
+function App() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const initWS = /*async*/ () => {
+      const socketProtocol =
+        window.location.protocol === "https:" ? "wss:" : "ws:";
+      const echoSocketUrl = `${socketProtocol}//localhost:9000/socket`;
+      const socket = new WebSocket(echoSocketUrl);
+      // const { data } = await axios.get(
+      //   "https://www.cloudflare.com/cdn-cgi/trace"
+      // );
+      // const ip = data.split("ip=")[1].split("ts=")[0];
+      // console.log(ip);
+      socket.onopen = () => {
+        console.log("open");
+        socket.send(`NEW client conexion`);
+      };
+
+      socket.onmessage = e => {
+        console.log("Message from server:", e.data);
+        setEvents([...events, e.data]);
+      };
+    };
+    initWS();
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="eventlist">
+        {events.map((value, index) => {
+          return <li key={index}>{value}</li>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
