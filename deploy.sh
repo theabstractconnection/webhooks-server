14fac6f3fc6be9e419ab9a10d64058593c82a81b
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-19 21:14:12 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,12 @@ set -e
 SERVER_USERNAME=$(echo $SERVER_USERNAME | tr -d '\r')
 PROJECT_NAME=$(echo $PROJECT_NAME | tr -d '\r')
 GIT_URL=$(echo $GIT_URL | tr -d '\r')
-GIT_URL=$(echo $GIT_URL | tr -d '\r')
+TARGET=$(echo $TARGET | tr -d '\r')
 
+echo $SERVER_USERNAME
+echo $PROJECT_NAME
+echo $GIT_URL
+echo $TARGET
 
 if [[ -z "${SERVER_USERNAME}" ]] || [[ -z "${PROJECT_NAME}" ]] || [[ -z "${GIT_URL}" ]]
 then
@@ -16,7 +20,7 @@ then
   exit 1
 else
   echo ">>> CD INTO /home/${SERVER_USERNAME}" 
-  cd /home/$SERVER_USERNAME
+  cd "/home/${SERVER_USERNAME}"
   if [[ -d $PROJECT_NAME ]] 
   then
     echo ">>> CD INTO ${PROJECT_NAME}" 
@@ -26,7 +30,7 @@ else
     if [ -z "$(git status --porcelain)" ]; then 
       echo ">>>>>> WORKING DIRECTORY CLEAN" 
       echo ">>> PULL FROM MASTER" 
-      git pull origin master
+      git pull origin master --progress
     else 
       echo ">>>>>> UNCOMMITED CHANGES"
       echo "!!!!!! EXITING"
@@ -38,10 +42,10 @@ else
   fi
 
   echo ">>> BUILDING SERVICES" 
-  make project_name=$PROJECT_NAME target=$TARGET build
+  make project_name=$PROJECT_NAME target=$TARGET build 2>&1
 
   echo ">>> STARTING SERICES"
-  make project_name=$PROJECT_NAME target=$TARGET service
+  make project_name=$PROJECT_NAME target=$TARGET service 2>&1
   
   echo ">>> SUCCCESS SERVICES STARTED"
 fi
