f8fcceea99b52c525ba805253577897cd12dbf30
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-19 13:39:58 +0100

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-APP_NAME=${PWD##*/} 
+APP_NAME="webhooks-server"
 PM2_EXECUTABLE="/home/ec2-user/.yarn/bin/pm2"
-APPLICATION_PATH="/home/ec2-user/projects/webhooks-server/"
+APPLICATION_PATH="/home/ec2-user/projects/${APP_NAME}/"
+
+echo "ENV : APP_NAME = ${APP_NAME}"
+echo "ENV : PM2_EXECUTABLE = ${PM2_EXECUTABLE}"
+echo "ENV : APPLICATION_PATH = ${APPLICATION_PATH}"
 
 echo ">>> START SCRIPT"
 cd "${APPLICATION_PATH}"
-
-
-echo ">>> APP_NAME = ${APP_NAME}"
-
 ${PM2_EXECUTABLE} describe "${APP_NAME}" > /dev/null
 RUNNING=$?
-
 if [ "${RUNNING}" -ne 0 ]; then
   echo ">>> PM2 START"
   ${PM2_EXECUTABLE} start server.js --name "${APP_NAME}"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-APP_NAME=${PWD##*/} 
+APP_NAME="webhooks-server"
 PM2_EXECUTABLE="/home/ec2-user/.yarn/bin/pm2"
 APPLICATION_PATH="/home/ec2-user/projects/webhooks-server/"
 
+APP_NAME="webhooks-server"
+PM2_EXECUTABLE="/home/ec2-user/.yarn/bin/pm2"
+APPLICATION_PATH="/home/ec2-user/projects/${APP_NAME}/"
+
+echo "ENV : APP_NAME = ${APP_NAME}"
+echo "ENV : PM2_EXECUTABLE = ${PM2_EXECUTABLE}"
+echo "ENV : APPLICATION_PATH = ${APPLICATION_PATH}"
+
 echo ">>> STOP SCRIPT"
 cd "${APPLICATION_PATH}"
-
-
 ${PM2_EXECUTABLE} stop "${APP_NAME}" || true
