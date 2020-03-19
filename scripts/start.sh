34785c8548eefe7e5a2d906df4f36ca60c625923
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-19 13:30:20 +0100

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-echo ">>> START SCRIPT"
-cd /home/ec2-user/projects/webhooks-server/
-
 APP_NAME=${PWD##*/} 
+PM2_EXECUTABLE="/home/ec2-user/.yarn/bin/pm2"
+APPLICATION_PATH="/home/ec2-user/projects/webhooks-server/"
+
+echo ">>> START SCRIPT"
+cd "${APPLICATION_PATH}"
+
+
 echo ">>> APP_NAME = ${APP_NAME}"
 
-pm2 describe "${APP_NAME}" > /dev/null
+${PM2_EXECUTABLE} describe "${APP_NAME}" > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
   echo ">>> PM2 START"
-  pm2 start server.js --name "${APP_NAME}"
+  ${PM2_EXECUTABLE} start server.js --name "${APP_NAME}"
 else
   echo ">>> PM2 RESTART"
-  pm2 restart "${APP_NAME}"
+  ${PM2_EXECUTABLE} restart "${APP_NAME}"
 fi;
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo ">>> STOP SCRIPT"
-cd /home/ec2-user/projects/webhooks-server/
-
 APP_NAME=${PWD##*/} 
-pm2 stop "${APP_NAME}" || true
+PM2_EXECUTABLE="/home/ec2-user/.yarn/bin/pm2"
+APPLICATION_PATH="/home/ec2-user/projects/webhooks-server/"
+
+echo ">>> STOP SCRIPT"
+cd "${APPLICATION_PATH}"
+
+
+${PM2_EXECUTABLE} stop "${APP_NAME}" || true
