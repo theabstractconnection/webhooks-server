83cba530927f1198d0a01e3f680938275586d9dc
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-22 15:39:32 +0100

    remove useless start of npm

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,9 +5,6 @@ APPLICATION_PATH="/home/ec2-user/projects/${APP_NAME}/"
 echo "ENV : APP_NAME = ${APP_NAME}"
 echo "ENV : APPLICATION_PATH = ${APPLICATION_PATH}"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 echo ">>> STOP SCRIPT"
 cd "${APPLICATION_PATH}"
 /opt/bin/pm2 stop "${APP_NAME}" || true
