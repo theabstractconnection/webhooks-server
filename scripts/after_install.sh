5e11b76dfcb221ed452d7a3690d44b2d3bcb4bfd
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-22 17:27:49 +0100

    fix missing / in path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,4 +3,4 @@ echo "☠☠☠ AFTER INSTALL SCRIPT"
 cd /home/ec2-user/envs
 git pull origin master 2>&1
 cd /home/ec2-user/projects/webhooks-server/scripts
-ln -s /home/ec2-user/envs/ecosystem.config.js home/ec2-user/projects/webhooks-server/ecosystem.config.js
+ln -s /home/ec2-user/envs/ecosystem.config.js /home/ec2-user/projects/webhooks-server/ecosystem.config.js
