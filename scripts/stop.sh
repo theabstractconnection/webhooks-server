6f5e6a2d16c749f160fb0246cb8a369fb8ff3942
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-22 17:40:51 +0100

    update stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -12,4 +12,11 @@ export NVM_DIR="$HOME/.nvm"
 
 echo "☠☠☠ PM2 STOP"
 cd "${APPLICATION_PATH}"
-/opt/bin/pm2 stop "${APP_NAME}" || true
+/opt/bin/pm2 describe "${APP_NAME}" > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+  echo "☠☠☠ NOT RUNNING"
+else
+  echo "☠☠☠ PM2 STOP"
+  /opt/bin/pm2 stop "${APP_NAME}" || true
+fi;
