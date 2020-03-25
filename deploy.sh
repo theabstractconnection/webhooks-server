13028da62d1ded93983a767839e171a227b4c9cb
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-25 17:47:51 +0100

    update deploy.sh with envs system

--- deploy.sh
+++ deploy.sh
@@ -22,35 +22,42 @@ echo $GIT_URL
 echo $TARGET
 echo $DEPLOY_TYPE
 
+echo "☠☠☠ UPDATING DOTENVS REPO"
+cd ${HOME}/dotenvs
+git pull origin master --progress
+cd ${HOME}/projects/webhooks-server
+
 if [[ -z "${SERVER_USERNAME}" ]] || [[ -z "${PROJECT_NAME}" ]] || [[ -z "${GIT_URL}" ]]
 then
   echo "☠☠☠ REQUIRED VARIABLES ARE NOT PRESENT"
   echo "!!! EXITING"
   exit 1
 else
-  if [[ -d $PROJECT_NAME ]] 
-  then
+  if [ "$DEPLOY_TYPE" == "initialize" ]; then
+    echo "☠☠☠ CLONING ${PROJECT_NAME}" 
+    git clone $GIT_URL 2>&1
+    cd $PROJECT_NAME
+    echo "☠☠☠ LINKING ENV FILE" 
+    envs -g
+  fi
+
+  if [ "$DEPLOY_TYPE" == "update" ]; then
     echo "☠☠☠ CD INTO ${PROJECT_NAME}" 
     cd $PROJECT_NAME
 
     echo "☠☠☠ CHECK GIT STATUS" 
     if [ -z "$(git status --porcelain)" ]; then 
-      echo "☠☠☠☠☠☠ WORKING DIRECTORY CLEAN" 
+      echo "☠☠☠ WORKING DIRECTORY CLEAN" 
       echo "☠☠☠ PULL FROM MASTER" 
       git pull origin master --progress
     else 
-      echo "☠☠☠☠☠☠ UNCOMMITED CHANGES"
-      echo "!!!!!! EXITING"
+      echo "☠☠☠ UNCOMMITED CHANGES"
+      echo "!!! EXITING"
       exit 1
     fi
-  else  
-    echo "☠☠☠ CLONING ${PROJECT_NAME}" 
-    git clone $GIT_URL 2>&1
-    cd $PROJECT_NAME
   fi
 
-  if [ "$DEPLOY_TYPE" == "initialize" ]
-  then
+  if [ "$DEPLOY_TYPE" == "initialize" ]; then
     echo "☠☠☠ PULLING SERVICE'S IMAGES" 
     make project_name=$PROJECT_NAME target=$TARGET pullimages 2>&1
   fi
@@ -61,8 +68,7 @@ else
   echo "☠☠☠ STARTING SERVICES"
   make project_name=$PROJECT_NAME target=$TARGET service 2>&1
   
-  if [ "$DEPLOY_TYPE" == "initialize" ]
-  then
+  if [ "$DEPLOY_TYPE" == "initialize" ]; then
     echo "☠☠☠ POST INSTALL SCRIPT" 
     make project_name=$PROJECT_NAME target=$TARGET postinstall 2>&1
   fi
