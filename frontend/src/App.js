84eec3c7e1d5849ff51e6fe91d5e13f1840d6808
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-28 20:52:04 +0100

    add keepalive & update storage logic for deploy & log events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,100 @@
 import React, { useState, useEffect, useRef } from "react";
-// import axios from "axios";
+
+import WebHook from '@component/WebHook';
 
 import "./App.css";
 
 function App() {
   const socket = useRef(null);
-  const [events, setEvents] = useState([]);
+  const timer = useReff(null);
+
+  const [deploys, setDeploys] = useState([{
+    deploy: {
+      data:{},
+      log:[],
+      status: ""
+    }
+  }]);
+  
+  const buildSocketUrl = () => {
+    const socketProtocol =
+      window.location.protocol === "https:" ? "wss:" : "ws:";
+    const hostname = window.location.hostname;
+    const socketUrl = `${socketProtocol}//${hostname}/socket`
+    console.log(socketUrl);
+    return socketUrl;
+  }
+
+  const keepAlive = (ws) => { 
+      const timeout = 20000;  
+      if (ws.readyState == ws.OPEN) {  
+          ws.send('');  
+      }
+      timer = setTimeout(keepAlive, timeout);  
+  }  
+  const cancelKeepAlive = () => {  
+      if (timer) {  
+          clearTimeout(timer);  
+      }  
+  }
+
+  const onDeployMsg = (msg) => {
+    if (msg.event === 'deploy'){
+      setDeploys(deploys => 
+        deploys.concat({
+          deploy: {
+            data: msg.githubInfos,
+            log:[],
+            status: "pending"
+          }
+        })
+      );
+    } 
+  };
+  const onLogMsg = (msg) => {
+    if ( msg.event === "log" ) {
+      setDeploys(deploys => 
+        deploys.map((deploy) => {
+          deploy.webhookDeliveryId === msg.webhookDeliveryId 
+            ? { ...deploy, log: deploy.log.concat(msg.data) }
+            : deploy
+        })
+      );
+    }
+  };
 
   useEffect(() => {
-    const initWS = /*async*/ () => {
+    const initWS = () => {
       if (socket.current) {
         return;
       }
-      const socketProtocol =
-        window.location.protocol === "https:" ? "wss:" : "ws:";
-      const hostname = window.location.hostname;
-      const socketUrl = `${socketProtocol}//${hostname}/socket`;
-      console.log(socketUrl);
-      let ws = new WebSocket(socketUrl);
+
+      let ws = new WebSocket(buildSocketUrl());
       socket.current = ws;
-      // const { data } = await axios.get(
-      //   "https://www.cloudflare.com/cdn-cgi/trace"
-      // );
-      // const ip = data.split("ip=")[1].split("ts=")[0];
-      // console.log(ip);
+
       ws.onopen = () => {
-        console.log("open");
-        ws.send(`NEW client conexion`);
+        keepAlive(ws);
+      };
+
+      ws.onclose = () => {
+        cancelKeepAlive();
       };
 
       ws.onmessage = (e) => {
-        console.log("Message from server:", e.data);
-        setEvents(event => event.concat(JSON.stringify(e.data)));
+        let msg = e.data;
+        onDeployMsg(msg);
+        onLogMsg(msg);
       };
     };
     initWS();
+
   }, []);
 
   return (
     <div className="App">
-      <div id="eventlist">
-        {events.map((value, index) => {
-          return <li key={index}>{value}</li>;
+      <div id="Deploys">
+        {deploys.map((deploy, index) => {
+          return <Deploy key={index} deploy={deploy}/>
         })}
       </div>
     </div>
