f940ba653a14ed92279d287a2a550210cf29ccf0
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-28 20:52:27 +0100

    add Deploy component

---
+++ frontend/src/components/Deploy.js
@@ -0,0 +1,11 @@
+import React/*, { useState, useEffect, useRef }*/ from "react";
+
+function Deploy() {
+  return (
+    <div id="Deploy">
+      {JSON.stringify(deploy)}
+    </div>
+  )
+}
+
+export default Deploy;
