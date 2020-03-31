361537217977a7019db92305a59e999eb97d9603
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-31 10:39:56 +0200

    init tailwind with postcss for base & emotion + tailwind.macro

--- frontend/src/components/Deployment.js
+++ frontend/src/components/Deployment.js
@@ -1,6 +1,21 @@
 import React /*, { useState, useEffect, useRef }*/ from 'react'
 import PropTypes from 'prop-types'
 
+import styled from '@emotion/styled'
+import tw from 'tailwind.macro'
+
+const Card = styled.div`
+  ${tw`
+      bg-gray-200 text-xl max-w-sm w-full lg:max-w-full lg:flex
+  `}
+`
+const CardPicture = styled.img`
+  ${tw`
+    h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t 
+    lg:rounded-t-none lg:rounded-l text-center overflow-hidden
+  `}
+`
+
 const Deployment = props => {
   const {
     webhookDeliveryId,
@@ -10,25 +25,35 @@ const Deployment = props => {
   } = props.deployment
 
   return (
-    <div id="Deployment">
+    <Card id="Deployment">
+      <CardPicture src={repository.owner.avatar_url} />
+      <div class="text-gray-900 font-bold text-xl mb-2">{repository.name}</div>
+      <p class="text-sm text-gray-600 flex items-center">
+        {repository.owner.name}
+      </p>
+      <p class="text-sm text-gray-600 flex items-center">
+        {repository.description}
+      </p>
+      <div class="flex items-center">
+        <img src={sender.avatar_url}  class="w-10 h-10 rounded-full mr-4" alt="Avatar" />
+        <div class="text-sm">
+          <p class="text-gray-900 leading-none">{sender.login}</p>
+          <p class="text-gray-600">{sender.email}</p>
+        </div>
+      </div>
+
+      <div className="repository">
+        <div>{repository.html_url}</div>
+      </div>
       <div>{webhookDeliveryId}</div>
       <div>{status}</div>
-      <div className="repository">
-        <div>{repository.name}</div>
-        <div>{repository.owner.name}</div>
-        <img style={{width: "30px", height: "30px"}} src={repository.owner.avatar_url} />
-        <div>{repository.html_url}</div>
-        <div>{repository.description}</div>
-      </div>
-      <div className="sender">
-        <div>{sender.login}</div>
-        <img style={{width: "30px", height: "30px"}} src={sender.avatar_url} />
-      </div>
       <div className="commits">
         {commits.map(comit => (
           <div>
             <div>{comit.id}</div>
-            <div>{comit.url}</div>
+            <a href={comit.url} target="_blank" rel="noopener noreferer">
+              {comit.message}
+            </a>
           </div>
         ))}
       </div>
@@ -40,7 +65,7 @@ const Deployment = props => {
           </div>
         ))}
       </div>
-    </div>
+    </Card>
   )
 }
 
