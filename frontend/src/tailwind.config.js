707375317a1b5cda4837194159f5ef916e10368c
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-01 00:42:55 +0200

    add screens

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   theme: {
     extend: {},
+    screens: {
+      'sm':  '576px',
+      'md':  '768px',
+      'lg':  '992px',
+      'xl':  '1200px',
+      // aliases
+      'wrist':  '576px',
+      'palm':  '768px',
+      'lap':  '992px',
+      'desk':  '1200px',
+      'wall': '2560px'
+    }
   },
   variants: {},
   plugins: [],
+
 }
