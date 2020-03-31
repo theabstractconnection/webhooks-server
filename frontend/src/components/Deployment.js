ecb6ff4a0f5823d01370e025df1594040c3c6b92
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-31 20:29:11 +0200

    update Deployment with new subcomponents

--- frontend/src/components/Deployment.js
+++ frontend/src/components/Deployment.js
@@ -1,70 +1,54 @@
-import React /*, { useState, useEffect, useRef }*/ from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-import styled from '@emotion/styled'
-import tw from 'tailwind.macro'
+import {
+  Card,
+  CardPicture,
+  CardContent,
+  CardContentHeader,
+  CardContentFooter,
+} from './deployment/Card'
 
-const Card = styled.div`
-  ${tw`
-      bg-gray-200 text-xl max-w-sm w-full lg:max-w-full lg:flex
-  `}
-`
-const CardPicture = styled.img`
-  ${tw`
-    h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t 
-    lg:rounded-t-none lg:rounded-l text-center overflow-hidden
-  `}
-`
+import Summary from './deployment/Summary'
+import Repository from './deployment/Repository'
+import Logs from './deployment/Logs'
+import AdditionalInfos from './deployment/AdditionalInfos'
 
 const Deployment = props => {
+  const [expanded, setExpanded] = useState(false)
+  const handleExpand = () => setExpanded(!expanded)
   const {
     webhookDeliveryId,
-    data: { repository, sender, commits },
+    data: { repository, sender, pusher, commits },
     logs,
     status,
   } = props.deployment
 
   return (
-    <Card id="Deployment">
+    <Card className="Deployment" onClick={handleExpand}>
       <CardPicture src={repository.owner.avatar_url} />
-      <div class="text-gray-900 font-bold text-xl mb-2">{repository.name}</div>
-      <p class="text-sm text-gray-600 flex items-center">
-        {repository.owner.name}
-      </p>
-      <p class="text-sm text-gray-600 flex items-center">
-        {repository.description}
-      </p>
-      <div class="flex items-center">
-        <img src={sender.avatar_url}  class="w-10 h-10 rounded-full mr-4" alt="Avatar" />
-        <div class="text-sm">
-          <p class="text-gray-900 leading-none">{sender.login}</p>
-          <p class="text-gray-600">{sender.email}</p>
-        </div>
-      </div>
-
-      <div className="repository">
-        <div>{repository.html_url}</div>
-      </div>
-      <div>{webhookDeliveryId}</div>
-      <div>{status}</div>
-      <div className="commits">
-        {commits.map(comit => (
-          <div>
-            <div>{comit.id}</div>
-            <a href={comit.url} target="_blank" rel="noopener noreferer">
-              {comit.message}
-            </a>
-          </div>
-        ))}
-      </div>
-      <div className="logs">
-        {logs.map(log => (
-          <div>
-            <div>{log.type}</div>
-            <div>{log.output}</div>
-          </div>
-        ))}
-      </div>
+      <CardContent>
+        <CardContentHeader status={status}>
+          <Summary
+            expanded={expanded}
+            expandHandler={handleExpand}
+            status={status}
+          />
+          <Repository
+            expanded={expanded}
+            repository={repository}
+          />
+          <AdditionalInfos
+            commits={commits}
+            expanded={expanded}
+            user={{ ...sender, email: pusher.email }}
+            webhookDeliveryId={webhookDeliveryId}
+          />
+        </CardContentHeader>
+        <CardContentFooter>
+          {expanded && <Logs logs={logs} expanded={expanded} />}
+        </CardContentFooter>
+      </CardContent>
     </Card>
   )
 }
