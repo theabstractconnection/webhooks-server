b1e78f90907432f8c429e1b9f452eb3850c88fc0
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-01 21:23:10 +0200

    update class to className

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,24 +4,24 @@ import networkPicture from '../assets/images/network.jpg'
 
 const Main = () => {
   return (
-    <div class="relative w-full flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2 bg-cover">
-      <div class="z-10">
-        <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">
+    <div className="relative w-full flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2 bg-cover">
+      <div className="z-10">
+        <h2 className="text-3xl font-semibold text-gray-800 md:text-4xl">
           Homemade{' '}
-          <span class="text-indigo-600">Webhooks deployment server</span>
+          <span className="text-indigo-600">Webhooks deployment server</span>
         </h2>
-        <p class="mt-2 text-sm text-whitemd:text-base">
+        <p className="mt-2 text-sm text-whitemd:text-base">
           Built with React Express Mongo Docker and more...
         </p>
-        <div class="flex justify-center lg:justify-start mt-6">
+        <div className="flex justify-center lg:justify-start mt-6">
           <Link
-            class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800"
+            className="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800"
             to="/deployments"
           >
             View recent deployments
           </Link>
           <Link
-            class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400"
+            className="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400"
             to="/about"
           >
             Learn More
@@ -29,7 +29,7 @@ const Main = () => {
         </div>
       </div>
       {/* <div
-        class="absolute lg:hidden top-0 left-0 w-full h-full opacity-75"
+        className="absolute lg:hidden top-0 left-0 w-full h-full opacity-75"
         style={{
           'background-image': `url(${networkPicture})`,
         }}
@@ -41,11 +41,11 @@ const Main = () => {
 const ClipSplash = () => {
   return (
     <div
-      class="hidden lg:block lg:w-1/2"
+      className="hidden lg:block lg:w-1/2"
       style={{ 'clip-path': 'polygon(10% 0, 100% 0%, 100% 100%, 0 100%)' }}
     >
       <div
-        class="h-full object-cover bg-cover opacity-75"
+        className="h-full object-cover bg-cover opacity-75"
         style={{
           'background-image': `url(${networkPicture})`,
         }}
@@ -57,7 +57,7 @@ const ClipSplash = () => {
 
 const Header = () => {
   return (
-    <div class="flex" style={{ height: '600px' }}>
+    <div className="flex" style={{ height: '600px' }}>
       <Main />
       <ClipSplash />
     </div>
