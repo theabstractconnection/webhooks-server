fe916a4fc8f89a4de6c17753ca844090e99ae9ce
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-01 21:21:45 +0200

    update with customize-cra

---
+++ frontend/config-overrides.js
@@ -0,0 +1,42 @@
+const {
+  // addBabelPlugin,
+  addDecoratorsLegacy,
+  addPostcssPlugins,
+  disableEsLint,
+  override,
+  overrideDevServer,
+  watchAll,
+} = require('customize-cra')
+
+const webpack = require('webpack')
+
+module.exports = {
+  webpack: override(
+    // enable legacy decorators babel plugin
+    addDecoratorsLegacy(),
+
+    // disable eslint in webpack
+    disableEsLint(),
+    // addBabelPlugin([
+    //   'tailwind-components',
+    //   {
+    //     config: './src/tailwind.config.js',
+    //     format: 'auto',
+    //   },
+    // ]),
+    addPostcssPlugins([
+      require('postcss-import')({
+        plugins: [require('stylelint')],
+      }),
+      require('tailwindcss')('./src/tailwind.config.js'),
+      require('postcss-preset-env')({
+        autoprefixer: { grid: true },
+        features: {
+          'nesting-rules': true,
+        },
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'],
+      }),
+    ])
+  ),
+  devServer: overrideDevServer(watchAll()),
+}
