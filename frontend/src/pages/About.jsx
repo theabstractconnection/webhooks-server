f965e18ec58bd2eed84c23404a324dac2c5e8d00
Manuel Wegria <abstract.s33d@gmail.com>
2020-04-08 17:21:17 +0200

    style: refacto

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import AWSLogo from '~/assets/images/aws.png'
@@ -13,6 +14,46 @@ import ReactLogo from '~/assets/images/react.png'
 import TailwindLogo from '~/assets/images/tailwind.png'
 import WebSocketLogo from '~/assets/images/websocket.png'
 
+const imageShape = PropTypes.arrayOf(
+  PropTypes.arrayOf(PropTypes.string.isRequired, PropTypes.string.isRequired)
+).isRequired
+
+const LogoArray = (props) => {
+  const { images } = props
+  return (
+    <div className="mt-3 flex flex-row flex-wrap items-center justify-center">
+      {images.map((image) => (
+        <img
+          key={image[0]}
+          alt={image[0]}
+          className="m-3 h-20"
+          src={image[1]}
+        />
+      ))}
+    </div>
+  )
+}
+
+LogoArray.propTypes = {
+  images: imageShape,
+}
+
+const Section = (props) => {
+  const { title, children, images } = props
+  return (
+    <div className="mt-3 mb-6">
+      <div className="text-2xl font-semibold text-gray-800">{title}</div>
+      <p className="text-gray-700">{children && children}</p>
+      <LogoArray images={images} />
+    </div>
+  )
+}
+
+Section.propTypes = {
+  images: imageShape,
+  title: PropTypes.string.isRequired,
+}
+
 const About = () => {
   return (
     <div className="w-full flex items-center text-center lg:text-left px-8 md:px-12">
@@ -22,87 +63,63 @@ const About = () => {
           <div className="text-indigo-600">Webhooks deployment server</div>
         </h2>
         <div className="mt-2 mb-20 text-sm text-whitemd:text-base max-w-3xl">
-          <div className="mt-3 mb-6">
-            <div className="text-2xl font-semibold text-gray-800">Hosting</div>
-            <p className="text-gray-700">
-              Hosted on automated aws ec2 instances (setup of instances is fully
-              automated by{' '}
-              <a
-                className="text-blue-400 underline hover:font-semibold"
-                href="https://github.com/theabstractconnection/configs/blob/master/ec2-setup.sh"
-                rel="noopener noreferrer"
-              >
-                this script
-              </a>
-              )
-            </p>
-            <div className="mt-3 flex flex-row flex-wrap items-center justify-center">
-              <img alt="Logo" className="m-3 h-20" src={AWSLogo} />
-            </div>
-          </div>
-          <div className="mt-3 mb-6">
-            <div className="text-2xl font-semibold text-gray-800">
-              Infrastructure
-            </div>
-            <p className="text-gray-700">
-              Custom Bash script that trigger Make commands (see{' '}
-              <a
-                className="text-blue-400 underline hover:font-semibold"
-                href="https://github.com/theabstractconnection/docker-compose-makefile"
-                rel="noopener noreferrer"
-              >
-                this repository
-              </a>{' '}
-              for details) to pull/build and start (as services) dockerized
-              projects with docker-compose. Projects are mapped to subdomains
-              with Nginx reverse proxy
-            </p>
-            <div className="mt-3 flex flex-row flex-wrap items-center justify-center">
-              <img alt="Bash Logo" className="m-3 h-20" src={BashLogo} />
-              <img alt="Make Logo" className="m-3 h-20" src={MakeLogo} />
-              <img alt="Docker Logo" className="m-3 h-20" src={DockerLogo} />
-              <img alt="Nginx Logo" className="m-3 h-20" src={NginxLogo} />
-            </div>
-          </div>
-          <div className="mt-3 mb-6">
-            <div className="text-2xl font-semibold text-gray-800">
-              Backend Server
-            </div>
-            <p className="text-gray-700">
-              NodeJs backend server built with ExpressJs and MongoDB (it also
-              spawn a websocket server to broadcast deploments and deployments
-              logs)
-            </p>
-            <div className="mt-3 flex flex-row flex-wrap items-center justify-center">
-              <img alt="Node Logo" className="m-3 h-20" src={NodeLogo} />
-              <img alt="Express Logo" className="m-3 h-20" src={ExpressLogo} />
-              <img alt="MongoDB Logo" className="m-3 h-20" src={MongoDbLogo} />
-            </div>
-          </div>
-          <div className="mt-3 mb-6">
-            <div className="text-2xl font-semibold text-gray-800">
-              Frontend Application
-            </div>
-            <p className="text-gray-700">
-              Create React App that use Websocket (to provide live logging
-              capabilities). Styling made with Tailwind CSS
-            </p>
-            <div className="mt-3 flex flex-row flex-wrap items-center justify-center">
-              <img alt="JS Logo" className="m-3 h-20" src={JSLogo} />
-              <img alt="React Logo" className="m-3 h-20" src={ReactLogo} />
-              <img
-                alt="WebSocket Logo"
-                className="m-3 h-20"
-                src={WebSocketLogo}
-              />
-              <img
-                alt="Tailwind Logo"
-                className="m-3 h-20"
-                src={TailwindLogo}
-              />
-            </div>
-          </div>
-          <div className="flex justify-center lg:justify-start mt-6"></div>
+          <Section images={[['AWS Logo', AWSLogo]]} title="Hosting">
+            Hosted on automated aws ec2 instances (setup of instances is fully
+            automated by{' '}
+            <a
+              className="text-blue-400 underline hover:font-semibold"
+              href="https://github.com/theabstractconnection/configs/blob/master/ec2-setup.sh"
+              rel="noopener noreferrer"
+            >
+              this script
+            </a>
+            )
+          </Section>
+          <Section
+            images={[
+              ['Bash Logo', BashLogo],
+              ['Make Logo', MakeLogo],
+              ['Docker Logo', DockerLogo],
+              ['Nginx Logo', NginxLogo],
+            ]}
+            title="Infrastructure"
+          >
+            Custom Bash script that trigger Make commands (see{' '}
+            <a
+              className="text-blue-400 underline hover:font-semibold"
+              href="https://github.com/theabstractconnection/docker-compose-makefile"
+              rel="noopener noreferrer"
+            >
+              this repository
+            </a>{' '}
+            for details) to pull/build and start (as services) dockerized
+            projects with docker-compose. Projects are mapped to subdomains with
+            Nginx reverse proxy
+          </Section>
+          <Section
+            images={[
+              ['Node Logo', NodeLogo],
+              ['ExpressLogo', ExpressLogo],
+              ['MongoDbLogo', MongoDbLogo],
+            ]}
+            title="Backend Server"
+          >
+            NodeJs backend server built with ExpressJs and MongoDB (it also
+            spawn a websocket server to broadcast deploments and deployments
+            logs)
+          </Section>
+          <Section
+            images={[
+              ['JS Logo', JSLogo],
+              ['React Logo', ReactLogo],
+              ['WebSocket Logo', WebSocketLogo],
+              ['Tailwind Logo', TailwindLogo],
+            ]}
+            title="Frontend Application"
+          >
+            Create React App that use Websocket (to provide live logging
+            capabilities). Styling made with Tailwind CSS
+          </Section>
         </div>
       </div>
     </div>
